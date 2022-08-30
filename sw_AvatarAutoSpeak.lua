582ba32bd95f7a80753cfc9603b11b22e104bef2
swefpifh <90806985+swefpifh@users.noreply.github.com>
2022-08-30 15:04:17 +0200

    Add files via upload

---
+++ sw_AvatarAutoSpeak.lua
@@ -0,0 +1,29 @@
+return function()
+	load()
+	
+	local maBubbleTextActivated = data.myAvatarBubbleTextActivated
+	
+	local maBubbleTextInfinite = data.myAvatarBubbleTextInfinite
+	local maBubbleTextLoop = data.myAvatarBubbleTextLoop
+	
+	local maBubbleTextInterval = math.random(data.myAvatarBubbleTextInterval.Min, data.myAvatarBubbleTextInterval.Max)
+	local maAvatar = getUser(data.myAvatarUsername)
+		
+	if maBubbleTextActivated == true then
+		if maBubbleTextInfinite == true then
+			while(true) do
+				local maBubbleText = data.myAvatarBubbleText[math.random(#data.myAvatarBubbleText)]
+				
+				wait(maBubbleTextInterval)
+				maAvatar.chatBubble(maBubbleText)
+			end
+		else
+			for i=1,maBubbleTextLoop do
+				local maBubbleText = data.myAvatarBubbleText[math.random(#data.myAvatarBubbleText)]
+				
+				wait(maBubbleTextInterval)
+				maAvatar.chatBubble(maBubbleText)
+			end
+		end
+	end
+end
